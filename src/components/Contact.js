0b7a606939fb430926f63dd141ec0ee4f989ab0c
Stanimir <stanimirkosev00@gmail.com>
2022-06-11 09:56:26 +0300

    footer component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,32 @@
 import React from "react";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import stockPhoto2 from "../images/stock_photos/stock_photo2.png";
 
 function Contact() {
   return (
     <div className="contact">
-      <h1>Contact Me</h1>
-      <p>Lorem</p>
-      <p>Lorem</p>
+      <div className="contact-info">
+        <h1>Contact Me</h1>
+        <p>
+          Please get in touch if you think our work could be mutually
+          beneficial!
+        </p>
+        <div className="email-container">
+          <MailOutlineIcon />
+          <p>stanimirkosev00@gmail.com</p>
+        </div>
+        <div className="contact-footer">
+          <a href="https://twitter.com/stanimir_kosev">
+            <TwitterIcon className="material-ui-icon twitter-logo" />
+          </a>
+          <a href="https://github.com/StanimirKosev">
+            <GitHubIcon className="material-ui-icon github-footer" />
+          </a>
+        </div>
+      </div>
+      <img src={stockPhoto2} alt="" className="stock-photo-second" />
     </div>
   );
 }
